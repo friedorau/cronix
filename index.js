95e5d17191049d43207d8605263bbb6e50218fa5
friedorau <friedorau@web.de>
2025-09-04 23:34:42 +0200

    index.js aktualisieren

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.API_KEY
+  apiKey: process.env.OPENAI_API_KEY
 });
 
 const response = openai.responses.create({
